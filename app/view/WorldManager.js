117ceaa3f78ab88fc96a45219e29158f01a626af
Siroko <sirokosan@gmail.com>
2017-04-29 14:12:02 +0200

    add checkmobile and package

--- app/view/WorldManager.js
+++ app/view/WorldManager.js
@@ -91,7 +91,7 @@ WorldManager.prototype.setup = function(){
             specialMatcap: THREE.ImageUtils.loadTexture('assets/emerald.jpg'),
             special2Matcap: THREE.ImageUtils.loadTexture('assets/matcap_purple.jpg'),
             sizeSimulation: mobilecheck() ? 64 : 128,
-            isMobile: true,
+            isMobile: mobilecheck(),
             renderer: this.renderer
         } );
 
